038f392fdcd261134a5bc327fabec946cdfb04bc
Mitsunori Komatsu <komamitsu@gmail.com>
2012-11-03 21:55:46 +0900

    remove main's printing out the last evaluated expr

--- main.ml
+++ main.ml
@@ -1,7 +1,4 @@
 let () =
   let exprs = Parser.parse Sys.argv.(1) in
-  let evaled_expr = Eval.eval exprs in
-  match evaled_expr with 
-  | Some e -> print_endline (Expr.string_of_expr e)
-  | None -> ()
+  ignore (Eval.eval exprs)
 
